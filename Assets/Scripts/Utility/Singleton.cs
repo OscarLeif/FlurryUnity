2b6624c9ae879150f3076792eb16b0e4611cb40a
Oscar Leif <oscar.leif@gmail.com>
2017-06-07 20:07:53 -0500

    Simple Android Example Project.

---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T m_instance;
+
+    //Only to check if this already exist
+    public static T GetInstance()
+    {
+        return m_instance;
+    }
+
+    public static T Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = GameObject.FindObjectOfType<T>();
+
+                if (m_instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    m_instance = singleton.AddComponent<T>();
+                }
+            }
+            return m_instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this as T;
+            DontDestroyOnLoad (this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
